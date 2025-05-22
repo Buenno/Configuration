f48a741b137424cb224684858a57433a66dac72c
Toby Williams <git@iambueno.co.uk>
2025-05-22 14:50:21 +0100

    feat: added windows terminal settings and fixed symlinks

--- Powershell/profile.ps1
+++ Powershell/profile.ps1
@@ -33,3 +33,53 @@ function New-EmptyCustomObject {
   $customObject = [PSCustomObject][Ordered]@{}
   $customObject | Select-Object -Property $PropertyNames
 }
+
+Function Invoke-EntraSync {
+    <#
+    .SYNOPSIS
+        Syncronise on-prem AD with Entra 
+     
+     
+    .NOTES
+        Name: Invoke-EntraSync
+        Author: Toby Williams
+        Version: 1.0
+        DateCreated: 01/10/2024
+     
+     
+    .EXAMPLE
+        Invoke-EntraSync
+     
+    #>
+     
+    [CmdletBinding()]
+    [Alias("Start-ADSyncSyncCycle")]
+    [Alias("Invoke-ADSync")]
+    [Alias("Invoke-AzureADSync")]
+    param(
+        [Parameter(Position=0,mandatory=$true)]
+        [string[]] $Server
+    )
+    
+    BEGIN {
+        $ErrorActionPreference = "Stop"
+    }
+    
+    PROCESS {
+        try {
+            Write-Host "Starting Entra AD sync..."
+            $command = Invoke-Command -ComputerName $Server -ScriptBlock {Start-ADSyncSyncCycle -PolicyType delta}
+            if ($command.Result -eq "Success") {
+                Write-Host "Sync completed successfully" -ForegroundColor Green
+            }
+            else {
+                Write-Error "There was an issue executing the sync command. Check remote logs for more details." -ForegroundColor yellow
+            }
+        }
+        catch {
+            Write-Error "Sync failed. $_"
+        }
+    }
+    
+    END {}
+}
--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,54 @@
 #Requires -RunAsAdministrator
 
-$symlinks = @{
-  "$PROFILE.CurrentUserAllHosts"          = ".\Powershell\profile.ps1"
-  "$env:APPDATA\Code\User\settings.json"  = ".\VSCode\settings.json"
+$modules = @(
+  "psgsuite",
+  "posh-git",
+  "posh-ssh",
+  "terminal-icons",
+  "plaster",
+  "modulebuilder"
+)
+
+# Installing NuGet package provider...
+Write-Host "Installing NuGet"
+Install-PackageProvider -Name NuGet -Force | Out-Null
+
+# Install the CascadiaCode font pack for Oh My Posh
+Start-Sleep -Seconds 2
+& "$env:localappdata\Programs\oh-my-posh\bin\oh-my-posh.exe" font install CascadiaCode | Out-Null
+
+# Powershell profile parent folder. This works with files and symlinks.
+$profileDir = $PROFILE.CurrentUserCurrentHost | Split-Path
+$profileParentDir = $profileDir.Substring(0, $profileDir.LastIndexOf('\'))
+
+# Install modules
+Write-Host "Installing modules"
+foreach ($module in $modules){
+  Install-Module -Name $module -Scope CurrentUser -Force
 }
+
+# Copy Modules dir from current Powershell env to alternative version env
+Write-Host "Copying modules to all Powershell instances"
+if ($PSVersionTable.PSVersion.Major -le '5'){
+  Copy-Item -Path "$profileDir\Modules\" -Recurse -Destination "$profileParentDir\PowerShell\"
+}
+elseif ($PSVersionTable.PSVersion.Major -eq '7'){
+  Copy-Item -Path "$profileDir\Modules\" -Recurse -Destination "$profileParentDir\WindowsPowerShell\"
+}
+
+$symlinks = @{
+  "$profileParentDir\WindowsPowershell\profile.ps1"    = "$PSScriptRoot\Powershell\profile.ps1"
+  "$profileParentDir\PowerShell\profile.ps1"    = "$PSScriptRoot\Powershell\profile.ps1"
+  "$env:APPDATA\Code\User\settings.json"  = "$PSScriptRoot\VSCode\settings.json"
+  "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" = "$PSScriptRoot\Windows Terminal\settings.json"
+}
+
+# Install Oh My Posh
+Write-Host "Installing Oh My Posh"
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1')) | Out-Null
+Start-Sleep -Seconds 5
+# Install the CascadiaCode front pack
+& "$env:localappdata\Programs\oh-my-posh\bin\oh-my-posh.exe" font install CascadiaCode | Out-Null
 
 # Create Symbolic Links
 Write-Host "Creating Symbolic Links..."
@@ -11,3 +56,9 @@ foreach ($symlink in $symlinks.GetEnumerator()) {
     Get-Item -Path $symlink.Key -ErrorAction SilentlyContinue | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
     New-Item -ItemType SymbolicLink -Path $symlink.Key -Target (Resolve-Path $symlink.Value) -Force | Out-Null
 }
+
+Write-Host -ForegroundColor Yellow "You may need to install Powershell 7 manually - https://learn.microsoft.com/en-gb/powershell/scripting/install/installing-powershell-on-windows?#msi"
+Write-Host -ForegroundColor Green "Setup complete"
+
+Write-Host "Press any key to continue"
+$void = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
